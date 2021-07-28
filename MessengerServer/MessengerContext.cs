59f47ff0a30d6797aed8d9c583475af1c87ccf28
Mark <Sobakad0@yandex.ru>
2021-07-28 22:33:43 +0400

    Init

---
+++ MessengerServer/MessengerContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MessengerServer.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessengerServer
+{
+    public class MessengerContext : DbContext, IMessengerContext
+    {
+        public virtual DbSet<UserDataModel> UserDataModels { get; set; }
+        public virtual DbSet<ContactDataModel> ContactDataModels { get; set; }
+        public virtual DbSet<MessageDataModel> MessageDataModels { get; set; }
+
+        public MessengerContext(DbContextOptions<MessengerContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public MessengerContext()
+        {
+        }
+
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=messengerserverdb;Trusted_Connection=True;");
+        // }
+    }
+}
---
+++ MessengerServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using MessengerServer.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessengerServer
+{
+    class Program
+    {
+        static void Main()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MessengerContext>();
+            var options = optionsBuilder
+                .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=messengerserverdb;Trusted_Connection=True;")
+                .Options;
+            var context = new MessengerContext(options);
+            var messageRepository = new MessageRepository(context);
+            var userRepository = new UserRepository(context);
+            var contactRepository = new ContactRepository(context, userRepository);
+        }
+    }
+}
